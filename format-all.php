740bfe1b4157879fcc61f72ca5abab36a2a4abbf
Marco Schuster <marco+github@m-s-d.eu>
2013-02-23 17:48:32 +0100

    add format-all.php which formats all code

---
+++ format-all.php
@@ -0,0 +1,40 @@
+#!/usr/bin/env php
+<?php
+
+//pre-commit checks
+
+//execute a command and check for return values
+function setup_exec($cmd, $return_out = false) {
+    $ret = 0;
+    $out = array();
+    $cmd .= " 2>&1";
+    printf("Launching %s\n", $cmd);
+    exec($cmd, $out, $ret);
+    if ($ret != 0) {
+        echo "-----\n" . implode("\n", $out) . "\n-----\n";
+        printf("'%s' failed. Aborting hook.\n", $cmd);
+        throw new Exception("");
+    }
+    if ($return_out)
+        return $out;
+}
+
+//always use the same basedir
+$scriptloc = realpath(dirname(__FILE__)) . "/";
+
+//format and check code
+$exit = 0;
+try {
+    $files = array();
+    $ret   = setup_exec("find " . escapeshellarg($scriptloc) . " -maxdepth 1 -type f -name \"*.php\"", true);
+    $files = array_merge($files, $ret);
+    $ret   = setup_exec("find " . escapeshellarg($scriptloc . "ftpfs/") . " -maxdepth 1 -type f -name \"*.php\"", true);
+    $files = array_merge($files, $ret);
+    foreach ($files as $file)
+        setup_exec("cd " . escapeshellarg($scriptloc) . " && ./format.php " . escapeshellarg($file));
+}
+catch (Exception $e) {
+    $exit = 1;
+}
+
+exit($exit);
